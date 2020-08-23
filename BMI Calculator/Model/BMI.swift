e83d0ed67237d36e1abf857d852b743b80b53d6b
Adwait Barkale <adwaitb@winjit.com>
2020-08-23 11:42:26 +0530

    Model to give advise

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Adwait Barkale on 23/08/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
